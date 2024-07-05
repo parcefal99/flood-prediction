b4bd27ea03344f4c3a8dd496d31ea1efb0196ee2
commanderxa <art.ostrikov@gmail.com>
2024-07-05 11:47:00 +0500

    train script enhancements

--- ML/train.py
+++ ML/train.py
@@ -7,29 +7,38 @@ from neuralhydrology.nh_run import start_run, continue_run
 
 
 def train():
-
+    # path to the config file
     cfg_path = Path("./conf/lstm.yml")
-    run_dir = Path(f"./runs/{sorted(os.listdir('./runs'))[-1]}")
 
+    # check if CUDA available
+    if torch.cuda.is_available():
+        pass
+    else:
+        raise Exception("No GPU found!")
+
+    # add CLI arguments
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--continue_train",
         action="store_true",
         default=False,
     )
-
     args = parser.parse_args()
 
+    # continue training if set so
     if args.continue_train:
+        try:
+            run_dir = Path(f"./runs/{sorted(os.listdir('./runs'))[-1]}")
+        except:
+            print(
+                "No `runs` directory found, first train a model from scrath without option `--continue_train`."
+            )
         print("Continue training")
         continue_run(run_dir=run_dir)
 
     else:
-        if torch.cuda.is_available():
-            start_run(config_file=cfg_path)
+        start_run(config_file=cfg_path)
 
-        else:
-            raise Exception("No GPU found!")
 
 
 if __name__ == "__main__":
